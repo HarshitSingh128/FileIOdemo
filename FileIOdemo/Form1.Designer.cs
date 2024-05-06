cbef43f438d754aff127f0ad8f8919daceb6e3c7
HarshitSingh128 <harshit3529@gmail.com>
2024-05-06 11:24:43 +0530

    Add project files.

---
+++ FileIOdemo/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace FileIOdemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnwrite = new System.Windows.Forms.Button();
+            this.btnread = new System.Windows.Forms.Button();
+            this.btccd = new System.Windows.Forms.Button();
+            this.btncf = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Id";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(183, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(183, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(183, 153);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(180, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Salary";
+            // 
+            // btnwrite
+            // 
+            this.btnwrite.Location = new System.Drawing.Point(66, 253);
+            this.btnwrite.Name = "btnwrite";
+            this.btnwrite.Size = new System.Drawing.Size(108, 30);
+            this.btnwrite.TabIndex = 6;
+            this.btnwrite.Text = "Write";
+            this.btnwrite.UseVisualStyleBackColor = true;
+            // 
+            // btnread
+            // 
+            this.btnread.Location = new System.Drawing.Point(245, 257);
+            this.btnread.Name = "btnread";
+            this.btnread.Size = new System.Drawing.Size(107, 29);
+            this.btnread.TabIndex = 7;
+            this.btnread.Text = "Read";
+            this.btnread.UseVisualStyleBackColor = true;
+            // 
+            // btccd
+            // 
+            this.btccd.Location = new System.Drawing.Point(418, 256);
+            this.btccd.Name = "btccd";
+            this.btccd.Size = new System.Drawing.Size(117, 30);
+            this.btccd.TabIndex = 8;
+            this.btccd.Text = "Create Directory";
+            this.btccd.UseVisualStyleBackColor = true;
+            // 
+            // btncf
+            // 
+            this.btncf.Location = new System.Drawing.Point(594, 253);
+            this.btncf.Name = "btncf";
+            this.btncf.Size = new System.Drawing.Size(107, 35);
+            this.btncf.TabIndex = 9;
+            this.btncf.Text = "Create File";
+            this.btncf.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 312);
+            this.Controls.Add(this.btncf);
+            this.Controls.Add(this.btccd);
+            this.Controls.Add(this.btnread);
+            this.Controls.Add(this.btnwrite);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnwrite;
+        private System.Windows.Forms.Button btnread;
+        private System.Windows.Forms.Button btccd;
+        private System.Windows.Forms.Button btncf;
+    }
+}
+
